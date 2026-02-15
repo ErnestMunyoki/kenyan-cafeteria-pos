cac687db7ec8ef8128d95fbba7afa2372bc4ee41
ErnestMunyoki <ernestmunyoki@gmail.com>
2026-02-15 17:24:04 +0300

    Remove problematic catch-all OPTIONS handler - use specific routes only

--- Backend/backend.cpp
+++ Backend/backend.cpp
@@ -212,21 +212,6 @@ int main() {
     loadSalesHistory();
     currentDateString = getCurrentDate();
 
-    // Catch-all OPTIONS handler
-    CROW_ROUTE(app, "<path>")
-    .methods("OPTIONS"_method)
-    ([](const crow::request& req){
-        crow::response res;
-        res.add_header("Access-Control-Allow-Origin", "*");
-        res.add_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        res.add_header("Access-Control-Allow-Headers", "Content-Type, Authorization, Origin, Accept");
-        res.add_header("Access-Control-Max-Age", "86400");
-        res.add_header("Access-Control-Allow-Credentials", "false");
-        res.add_header("Vary", "Origin");
-        res.code = 204;
-        return res;
-    });
-
     // OPTIONS handlers for preflight requests
     CROW_ROUTE(app, "/items").methods("OPTIONS"_method)
     ([](const crow::request& req){ 
